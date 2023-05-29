86d41c837a3417130070ec99b2884f13309174ac
Muhammet Şen <enmuhammet23@gmail.com>
2023-05-29 16:14:15 +0300

    change path of saver thread

--- utils/video_recorder.py
+++ utils/video_recorder.py
@@ -13,22 +13,31 @@ from utils.perfect_sleep import perfect_sleep
 import multiprocessing as mp
 from queue import Queue
 
-from saver import saver
+def saver(queue, filename):
+	VIDEO_CODEC = cv2.VideoWriter_fourcc(*"avc1")
+	VIDEO_FPS = 30
+	VIDEO_RESOLUTION = (1000, 1000)
+	out = cv2.VideoWriter(filename, VIDEO_CODEC, VIDEO_FPS, VIDEO_RESOLUTION)
+	while True:
+		frame = queue.get()
+		if frame is None: break
+		out.write(frame)
+	out.release()
 
 def record(fps = Config().VIDEO_FPS):
 	filename = f"{Config().TEMP_DIR}/{time.time()}{Config().FILE_EXTENSION}"
 	
-	# out: cv2.VideoWriter = cv2.VideoWriter(filename, Config().VIDEO_CODEC, fps, Config().VIDEO_RESOLUTION)
 	sct = mss()
 	bounding_box = {'top': 0, 'left': 0, 'width': Config().VIDEO_RESOLUTION[0], 'height': Config().VIDEO_RESOLUTION[1]}
 	
 	counter = 0
 
 	first_time = time.perf_counter()
-	# q = mp.Queue()
-	q= Queue()
+
+	q = Queue()
 	process = Thread(target=saver, args=(q, filename))
 	process.start()
+
 	while True:
 		if(counter == Config().VIDEO_DURATION * fps): break
 		counter += 1
@@ -44,12 +53,8 @@ def record(fps = Config().VIDEO_FPS):
 		frame_duration = time.perf_counter() - frame_start_time
 		perfect_sleep(1/fps - frame_duration)
 	q.put(None)
-	# process.join()
-	# out.write(np.array(outs))
-	# out.release()
-	if Config().COMPRESSION_ENABLED: filencxame = compress_video.compress(filename)
 
-	# print(f"Compression took {time.perf_counter() - s} seconds")
+	if Config().COMPRESSION_ENABLED: filename = compress_video.compress(filename)
+
 	print(f"Total time elapsed: {time.perf_counter() - first_time} seconds")
-	# print(f"file size: {os.path.getsize(filename)/1024/1024} mb")
 	return filename
